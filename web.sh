dfb79a8171fb2261cd433a20bde8035a593242cb
Evan Madow <evan@evanm.com>
2017-06-03 14:13:32 -0700

    no need for coffeescript

--- web.sh
+++ web.sh
@@ -23,7 +23,7 @@ brew install node
 # Remove outdated versions from the cellar.
 brew cleanup
 
-npm install -g coffee-script
+# npm install -g coffee-script
 npm install -g grunt-cli
 npm install -g jshint
 npm install -g less
